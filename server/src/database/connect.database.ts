91d1244d2591e6497b7395c4dd78ded5fe08266a
AgaevK <kenkosh@yandex.ru>
2023-10-03 20:36:06 +0300

    dockerizes node app

--- server/src/database/connect.database.ts
+++ server/src/database/connect.database.ts
@@ -1,12 +1,12 @@
 import { connect, set } from 'mongoose';
-import { DB_HOST, NODE_ENV, DB_PORT } from '@app/configs';
+import { DB_HOST, NODE_ENV, DB_PORT, DB_NAME } from '@app/configs';
 
 export const connectDatabase = async () => {
   if (NODE_ENV !== 'production') {
     set('debug', true);
   }
 
-  await connect(`mongodb://${DB_HOST}:${DB_PORT}`)
+  await connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
     .then(() => {
       console.log('Connection to DB established');
     })
